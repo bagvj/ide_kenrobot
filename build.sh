dd8f4ce222f4effee34c4debaa39cb4cd780f840
root <root@iZ25s4oqn8wZ.(none)>
2015-10-28 15:30:20 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo $#
+if [ $# -ne 1 ];then
+    exit 1
+fi
+STAMP=$1
+SOURCE=/tmp/$STAMP/CSource.cpp
+SOURCEPATH=/tmp/$STAMP/
+DATAHEADER=/alidata/server/avr/
+
+cp $DATAHEADER/* $SOURCEPATH
+cd $SOURCEPATH
+make
+exit 0
