5252cad34d7a9a9f2178eec2abdf5b93d3f37c0b
kite17 <wangkunpeng@kenrobot.com>
2015-11-09 13:22:14 +0800

    添加登录模版页面

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+<!-- resources/views/auth/login.blade.php -->
+
+<form method="POST" action="/auth/login">
+    {!! csrf_field() !!}
+
+    <div>
+        账户
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        密码
+        <input type="password" name="password" id="password">
+    </div>
+
+    <div>
+        <input type="checkbox" name="remember"> 记住我
+    </div>
+
+    <div>
+        <button type="submit">登录</button>
+    </div>
+</form>
