197a146fc5ed38f4b8809dbe1b98dd82276ee9ec
kite17 <wangkunpeng@kenrobot.com>
2015-11-16 22:17:30 +0800

    登录框嵌入到页面中

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,6 +21,39 @@
  <body>
  
    @yield('main')
-  <div id="login_dialog" style="display:none"></div>
+  <div id="login_dialog" style="display:none">
+   
+<div id="login-tabs">
+  <ul>
+    <li><a href="#login-tab-1">账号密码登录</a></li>
+    <li><a href="#login-tab-2">微信登录</a></li>
+  </ul>
+  <div id="login-tab-1">
+    <form method="POST" action="/snspostlogin" class="loginForm">
+        {!! csrf_field() !!}
+
+        <div class="field">
+          <label>账户</label>
+          <input type="email" name="email" value="{{ old('email') }}" />
+        </div>
+        <div class="field">
+            <label>密码</label>
+            <input type="password" name="password" id="password" />
+        </div>
+        <div class="remember">
+            <input type="checkbox" name="remember" /><label>记住我</label>
+        </div>
+        <div class="submit">
+            <input id="qrcode_key" type="hidden" value="{{$key or ''}}">
+            <input type="submit" value="登录"/>
+        </div>
+    </form>
+  </div>
+  <div id="login-tab-2">
+    <img alt="微信扫码" src="{{ $qrcodeurl or '' }}" style="width:200px;height:200px;" />
+  </div>
+</div>
+
+  </div>
  </body>
 </html>
