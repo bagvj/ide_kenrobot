6444580e948a0cb60538ea2ab234a7d75f0ffb3a
kite17 <wangkunpeng@kenrobot.com>
2016-02-26 15:30:52 +0800

    登录成功时携带用户信息

--- app/Http/Controllers/Auth/WeixinAuthController.php
+++ app/Http/Controllers/Auth/WeixinAuthController.php
@@ -74,7 +74,8 @@ class WeixinAuthController extends Controller
     {
         $key = $request->input('key');
         if (Auth::check()) {
-            return response()->json(['code' => 1, 'message' => '已经登录']);
+            $user = Auth::user();
+            return response()->json(['code' => 1, 'message' => '已经登录', 'data' => ['userid' => $user->id]]);
 
         }
 
@@ -95,7 +96,7 @@ class WeixinAuthController extends Controller
 
         }
         Auth::login($user,false);
-        return response()->json(['code' => 0, 'message' => '登录成功']);
+        return response()->json(['code' => 0, 'message' => '登录成功','data' => ['userid' => $user->id]]);
     }
 
    
