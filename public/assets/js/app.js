40b133952f246c7a975d149c9fde40badc708335
zhouguang <zhouguang@kenrobot.com>
2016-03-17 10:11:23 +0800

    pv统计

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,6 @@
 define(['jquery', 'config', 'hardware', 'user', 'project', 'software', 'sidebar', 'board', 'component', 'library'], function($, config, hardware, user, project, software, sidebar, board, component, library) {
 	function init() {
+		initPV();
 		initAjax();
 
 		user.init();
@@ -38,6 +39,14 @@ define(['jquery', 'config', 'hardware', 'user', 'project', 'software', 'sidebar'
 				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 			}
 		});
+	}
+
+	//pv统计
+	function initPV() {
+		var hm = document.createElement("script");
+		hm.src = "//hm.baidu.com/hm.js?6518098de0bee39bef219952dbbae669";
+		var s = document.getElementsByTagName("script")[0]; 
+		s.parentNode.insertBefore(hm, s);
 	}
 
 	function onLoadSuccess(result) {
