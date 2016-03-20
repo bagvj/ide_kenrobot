dbec7f02bf4bae11043e587e3ec1c272d94e3751
zhouguang <zhouguang@kenrobot.com>
2016-03-20 14:06:16 +0800

    css compress

--- resources/assets/build.sh
+++ resources/assets/build.sh
@@ -11,7 +11,7 @@ rm -rf ${PACK}
 r.js -o build.js
 
 cd ${PACK}
-rm -rf build.* *.rb *.sh css/*.scss .sass-cache
+rm -rf build.* *.rb *.sh .sass-cache css/*.scss css/base css/component css/helper css/layout css/theme
 
 #回到之前的目录
 cd ${DIR}
