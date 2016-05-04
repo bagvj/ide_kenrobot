d14fd99d535776778ce6863d65d76ad7318be570
kite17 <wangkunpeng@kenrobot.com>
2016-05-04 15:26:23 +0800

    bug:fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,8 @@
  */
 Route::get('/', 'HomeController@index');
 Route::get('/logout', 'Auth\AuthController@getLogout2');
-Route::get('/project/download/{hash}/{ext?}', 'ProjectController@downloadProject')->where('hash', '[0-9a-zA-Z]{6}');
+Route::get('/project/download/{hash}/{ext?}', 'NewProjectController@downloadProject')->where('hash', '[0-9a-zA-Z]{6}');
+
 
 // 临时
 Route::post('/temp/platformid', 'Auth\WebAuthController@platformId');
@@ -20,7 +21,7 @@ Route::post('/temp/platformid', 'Auth\WebAuthController@platformId');
 // 配置API
 Route::post('/api/config', 'HomeController@config');
 // 项目API
-Route::post('/api/project/build', 'ProjectController@buildProject');
+Route::post('/api/project/build', 'NewProjectController@buildProject');
 Route::post('/api/project/save', 'NewProjectController@saveProject');
 Route::post('/api/project/delete', 'NewProjectController@deleteProject');
 Route::post('/api/project/get', 'NewProjectController@getProject');
