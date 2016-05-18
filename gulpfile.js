e85d729a5a999582a2a9c3d3d36f293a5e2655f8
zhouguang <zhouguang@kenrobot.com>
2016-05-18 20:15:26 +0800

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
 /**
  * 组件安装
- * npm install gulp gulp-if gulp-rev gulp-rev-replace minimist run-sequence gulp-concat gulp-rename gulp-clean gulp-jshint gulp-uglify gulp-jsonminify gulp-ruby-sass gulp-clean-css gulp-imagemin gulp-minify-html gulp-ng-annotate gulp-sourcemaps gulp-requirejs-optimize --save-dev
+ * npm install gulp gulp-if gulp-rev gulp-rev-replace minimist run-sequence gulp-concat gulp-rename gulp-clean gulp-jshint gulp-uglify gulp-jsonminify gulp-ruby-sass gulp-clean-css gulp-autoprefixer gulp-imagemin gulp-minify-html gulp-ng-annotate gulp-sourcemaps gulp-requirejs-optimize --save-dev
  */
 
 // 引入 gulp及组件
-var gulp = require('gulp'),                   //基础库
-	gulpif = require('gulp-if');              //条件执行
-	rev = require('gulp-rev');                //rev
-	revReplace = require('gulp-rev-replace'); //rev替换
-	minimist = require('minimist')            //命令行参数解析
-	runSequence = require('run-sequence');    //顺序执行
-	concat = require('gulp-concat'),          //合并文件
-	rename = require('gulp-rename'),          //重命名
-	clean = require('gulp-clean');            //清空文件夹
-	jshint = require('gulp-jshint'),          //js检查
-	uglify = require('gulp-uglify'),          //js压缩
-	jsonminify = require('gulp-jsonminify');  //json压缩
-	sass = require('gulp-ruby-sass'),         //sass
-	cleanCSS = require('gulp-clean-css'),     //css压缩
-	imagemin = require('gulp-imagemin'),      //image压缩
-	minifyHtml = require("gulp-minify-html"); //html压缩
-	ngAnnotate = require('gulp-ng-annotate'), //ng注释
-	sourcemaps = require('gulp-sourcemaps'),  //source map
+var gulp = require('gulp'),                      //基础库
+	gulpif = require('gulp-if');                 //条件执行
+	rev = require('gulp-rev');                   //rev
+	revReplace = require('gulp-rev-replace');    //rev替换
+	minimist = require('minimist')               //命令行参数解析
+	runSequence = require('run-sequence');       //顺序执行
+	concat = require('gulp-concat'),             //合并文件
+	rename = require('gulp-rename'),             //重命名
+	clean = require('gulp-clean');               //清空文件夹
+	jshint = require('gulp-jshint'),             //js检查
+	uglify = require('gulp-uglify'),             //js压缩
+	jsonminify = require('gulp-jsonminify');     //json压缩
+	sass = require('gulp-ruby-sass'),            //sass
+	cleanCSS = require('gulp-clean-css'),        //css压缩
+	autoprefixer = require('gulp-autoprefixer'), //自动前缀
+	imagemin = require('gulp-imagemin'),         //image压缩
+	minifyHtml = require("gulp-minify-html");    //html压缩
+	ngAnnotate = require('gulp-ng-annotate'),    //ng注释
+	sourcemaps = require('gulp-sourcemaps'),     //source map
 	requirejsOptimize = require('gulp-requirejs-optimize'); //requirejs打包
 
 var SRC = './resources/assets/';
@@ -67,6 +68,7 @@ gulp.task('css', function() {
 		cssDst = DIST + 'css/';
 
 	return sass(cssSrc, {style: 'expanded'})
+		.pipe(gulpif(args.prefix, autoprefixer()))
 		.pipe(gulpif(args.release, cleanCSS()))
 		.pipe(gulp.dest(cssDst));
 });
