76711cad4a9965e8734792a3a18c146d32312ce5
zhouguang <zhouguang@kenrobot.com>
2016-07-14 15:29:47 +0800

    add more boards

---
+++ database/migrations/2016_07_14_072533_add_more_boards.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMoreBoards extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("insert  into `boards`(`name`,`label`,`board_type`,`in_use`,`is_forward`,`width`,`height`,`category`) values ('Arduino/Genuino 101','Arduino/Genuino 101','genuino101',1,0,392,273,'ArduinoUNO');");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
