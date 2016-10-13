57dd6f9ac21af947f907d85d864858224a727971
zhouguang <zhouguang@kenrobot.com>
2016-10-13 19:47:21 +0800

    update crx

--- app/Extension/src/js/app.js
+++ app/Extension/src/js/app.js
@@ -4,8 +4,17 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 	var serialDatas;
 	var encoder;
 	var decoder;
-	var DTR_RTS_ON = {dtr: true, rts: true}; 
-	var DTR_RTS_OFF = {dtr: false, rts: false};
+
+	var DTR_RTS = {
+		ON: {
+			dtr: true,
+			rts: true
+		},
+		OFF: {
+			dtr: false,
+			rts: false
+		}
+	};
 
 	function init() {
 		chrome.runtime.onMessageExternal.addListener(onMessageExternal);
@@ -21,7 +30,7 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 		try {
 			var count = 0;
 			var onDisconnect = function(result) {
-				if(count > 0) {
+				if (count > 0) {
 					count--;
 				} else {
 					callback();
@@ -29,8 +38,8 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 			}
 			chrome.serial.getConnections(function(connections) {
 				count = connections.length;
-				if(count > 0) {
-					for(var i = 0; i < count; i++) {
+				if (count > 0) {
+					for (var i = 0; i < count; i++) {
 						var connectionInfo = connections[i];
 						chrome.serial.disconnect(connectionInfo.connectionId, onDisconnect);
 					}
@@ -38,23 +47,23 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 					callback();
 				}
 			});
-		} catch(ex) {
+		} catch (ex) {
 			callback();
 		}
 	}
 
 	function onMessageExternal(message, sender, sendResponse) {
-		if(!message || !message.action) {
+		if (!message || !message.action) {
 			return;
 		}
 
 		var action = message.action;
-		if(action == "serial.getDevices") {
+		if (action == "serial.getDevices") {
 			chrome.serial.getDevices(function(ports) {
 				sendResponse(ports);
 			});
 			return true;
-		} else if(action == "serial.connect") {
+		} else if (action == "serial.connect") {
 			connectionId = null;
 			uploadProgress = 0;
 			serialDatas = [];
@@ -63,50 +72,46 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 					chrome.serial.connect(message.portPath, {
 						bitrate: message.bitRate,
 					}, function(connectionInfo) {
-						if(connectionInfo && connectionInfo.connectionId) {
+						if (connectionInfo && connectionInfo.connectionId) {
 							connectionId = connectionInfo.connectionId;
 							sendResponse(connectionId);
 						} else {
 							sendResponse(false);
 						}
 					});
-				} catch(ex) {
+				} catch (ex) {
 					sendResponse(false);
 				}
 			});
 			return true;
-		} else if(action == "serial.disconnect") {
+		} else if (action == "serial.disconnect") {
 			connectionId = null;
 			uploadProgress = 0;
 			serialDatas = [];
 			try {
-				chrome.serial.disconnect(message.connectionId, function(result){
+				chrome.serial.disconnect(message.connectionId, function(result) {
 					sendResponse(result);
 				});
-			} catch(ex) {
+			} catch (ex) {
 				sendResponse(true);
 			}
 			return true;
-		} else if(action == "serial.getInfo") {
+		} else if (action == "serial.getInfo") {
 			chrome.serial.getInfo(message.connectionId, function(connectionInfo) {
-				if(connectionInfo && connectionInfo.connectionId) {
+				if (connectionInfo && connectionInfo.connectionId) {
 					sendResponse(true);
 				} else {
 					sendResponse(false);
 				}
 			});
 			return true;
-		} else if(action == "serial.reset") {
-			chrome.serial.setControlSignals(connectionId, DTR_RTS_OFF, function(a) {
-				setTimeout(function() {
-					chrome.serial.setControlSignals(connectionId, DTR_RTS_ON, function(b) {
-						sendResponse(true);
-					});
-				}, 50);
+		} else if (action == "serial.reset") {
+			resetDTRRTS(function() {
+				sendResponse(true);
 			});
 			return true;
-		} else if(action == "serial.send") {
-			if(!connectionId) {
+		} else if (action == "serial.send") {
+			if (!connectionId) {
 				sendResponse(false);
 				return;
 			}
@@ -115,8 +120,8 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 				sendResponse(sendInfo);
 			});
 			return true;
-		} else if(action == "serial.flush") {
-			if(!connectionId) {
+		} else if (action == "serial.flush") {
+			if (!connectionId) {
 				sendResponse(false);
 				return;
 			}
@@ -124,35 +129,46 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 				sendResponse(result);
 			});
 			return true;
-		} else if(action == "serial.receive") {
-			if(!connectionId) {
+		} else if (action == "serial.receive") {
+			if (!connectionId) {
 				sendResponse(false);
 				return;
 			}
 			var result = serialDatas;
 			serialDatas = [];
 			sendResponse(result);
-		} else if(action == "upload") {
+		} else if (action == "upload") {
 			$.ajax({
 				url: message.url,
 			}).done(function(hexData) {
 				uploadProgress = 5;
-				upload.exec(connectionId, hexData, message.delay, function(success) {
-					sendResponse(success);
-				}, function(progress) {
-					uploadProgress = progress;
+
+				resetDTRRTS(function() {
+					setTimeout(function() {
+						upload.exec({
+							data: hexData,
+							delay: message.delay,
+							send: send,
+							onProgress: function(progress) {
+								uploadProgress = 15 + Math.round(85 * progress / 100);
+							},
+							onComplete: function(success) {
+								sendResponse(success);
+							}
+						});
+					}, 200);
 				});
 			}).fail(function() {
 				sendResponse(false);
 			});
 			return true;
-		} else if(action == "upload.progress") {
+		} else if (action == "upload.progress") {
 			sendResponse(uploadProgress);
 		}
 	}
 
 	function onReceive(info) {
-		if(connectionId && info.connectionId == connectionId) {
+		if (connectionId && info.connectionId == connectionId) {
 			var str = decoder.decode(new Uint8Array(info.data));
 			serialDatas.push(str);
 		}
@@ -164,10 +180,34 @@ define(['jquery', './encoding', './upload'], function($, _, upload) {
 		serialDatas = [];
 
 		try {
-			chrome.serial.disconnect(connectionId, function(){});
-		} catch(ex) {
+			chrome.serial.disconnect(connectionId, function() {});
+		} catch (ex) {
 
 		}
+	}
+
+	function resetDTRRTS(callback) {
+		chrome.serial.setControlSignals(connectionId, DTR_RTS.OFF, function(a) {
+			setTimeout(function() {
+				chrome.serial.setControlSignals(connectionId, DTR_RTS.ON, function(b) {
+					callback();
+				});
+			}, 50);
+		});
+	}
+
+	function stringToBuffer(str) {
+		var bytes = new Uint8Array(str.length);
+		for (var i = 0; i < str.length; ++i) {
+			bytes[i] = str.charCodeAt(i);
+		}
+		return bytes.buffer;
+	}
+
+	function send(message, callback) {
+		chrome.serial.send(connectionId, stringToBuffer(message), function(result) {
+			callback();
+		});
 	}
 
 	return {
--- app/Extension/src/js/upload.js
+++ app/Extension/src/js/upload.js
@@ -1,104 +1,82 @@
 define(function() {
-	var connectionId;
-	var hexData;
-	var completeCallback;
-	var progressCallback;
+	var d2b = String.fromCharCode;
 
-	var timer;
-	var burnCount;
-	var burnTotal;
-
-	var DTRRTSOn = {dtr: true, rts: true}; 
-	var DTRRTSOff = {dtr: false, rts: false};
-	var command = {
-		"Sync_CRC_EOP" : 0x20,
-		"GET_SYNC" : 0x30,
-		"GET_SIGN_ON" : 0x31,
-		"SET_PARAMETER" : 0x40,
-		"GET_PARAMETER" : 0x41,
-		"SET_DEVICE" : 0x42,
-		"SET_DEVICE_EXT" : 0x45,
-		"ENTER_PROGMODE" : 0x50,
-		"LEAVE_PROGMODE" : 0x51,
-		"CHIP_ERASE" : 0x52,
-		"CHECK_AUTOINC" : 0x53,
-		"LOAD_ADDRESS" : 0x55,
-		"UNIVERSAL" : 0x56,
-		"UNIVERSAL_MULTI" : 0x57,
-		"PROG_FLASH" : 0x60,
-		"PROG_DATA" : 0x61,
-		"PROG_FUSE" : 0x62,
-		"PROG_LOCK" : 0x63,
-		"PROG_PAGE" : 0x64,
-		"PROG_FUSE_EXT" : 0x65,
-		"READ_FLASH" : 0x70,
-		"READ_DATA" : 0x71,
-		"READ_FUSE" : 0x72,
-		"READ_LOCK" : 0x73,
-		"READ_PAGE" : 0x74,
-		"READ_SIGN" : 0x75,
-		"READ_OSCCAL" : 0x76,
-		"READ_FUSE_EXT" : 0x77,
-		"READ_OSCCAL_EXT" : 0x78
+	var COMMAND = {
+		SYNC_CRC_EOP: 0x20,
+		GET_SYNC: 0x30,
+		GET_SIGN_ON: 0x31,
+		SET_PARAMETER: 0x40,
+		GET_PARAMETER: 0x41,
+		SET_DEVICE: 0x42,
+		SET_DEVICE_EXT: 0x45,
+		ENTER_PROG_MODE: 0x50,
+		LEAVE_PROG_MODE: 0x51,
+		CHIP_ERASE: 0x52,
+		CHECK_AUTOINC: 0x53,
+		LOAD_ADDRESS: 0x55,
+		UNIVERSAL: 0x56,
+		UNIVERSAL_MULTI: 0x57,
+		PROG_FLASH: 0x60,
+		PROG_DATA: 0x61,
+		PROG_FUSE: 0x62,
+		PROG_LOCK: 0x63,
+		PROG_PAGE: 0x64,
+		PROG_FUSE_EXT: 0x65,
+		READ_FLASH: 0x70,
+		READ_DATA: 0x71,
+		READ_FUSE: 0x72,
+		READ_LOCK: 0x73,
+		READ_PAGE: 0x74,
+		READ_SIGN: 0x75,
+		READ_OSCCAL: 0x76,
+		READ_FUSE_EXT: 0x77,
+		READ_OSCCAL_EXT: 0x78
 	};
 
-	function exec(connId, data, burnDelay, complete, progress) {
-		connectionId = connId;
-		timer = 0;
-		completeCallback = complete;
-		progressCallback = progress;
-		burnTotal = 0;
-		burnCount = 0;
+	var completeCallback;
+	var progressCallback;
+	var packageCount;
+	var packageTotal;
+	var timers = [];
+
+	function exec(args) {
+		var delay = args.delay || 250;
+		var hexRaw = args.data;
+		var send = args.send;
+		completeCallback = args.onComplete;
+		progressCallback = args.onProgress;
+
+		var packages = convertHex(hexRaw);
+		packageTotal = packages.length;
+		packageCount = 0;
 
 		try {
-			//准备数据
-			prepare(data);
-			updateProgress(10);
-
-			//先关，等50ms后再开。然后200ms后正式发送数据
-			chrome.serial.setControlSignals(connectionId, DTRRTSOff, function(result) {
-				setTimeout(function() {
-					chrome.serial.setControlSignals(connectionId, DTRRTSOn, function(result) {
-						setTimeout(function() {
-							doUpload(burnDelay);
-						}, 200);
-					});
-				}, 50);
-			});
-		} catch(ex) {
-			console.dir(ex);
-			doComplete(false);
+			var time = doSend(send, packages[0], 50);
+			for (var i = 1; i < packages.length; i++) {
+				time = doSend(send, packages[i], time + delay);
+			}
+		} catch (ex) {
+			doComplete(false, ex);
 		}
 	}
 
-	function prepare(data) {
-		hexData = "";
-		var buffer = data.split("\n");
-		for (var x = 0; x < buffer.length; x++) {
-			var size = parseInt(buffer[x].substr(1, 2), 16);
-			if (size == 0) {
-				break;
-			}
-			for (var y = 0; y < (size * 2); y = y + 2) {
-				hexData += String.fromCharCode(parseInt(buffer[x].substr(y + 9, 2), 16));
-			}
-		}
+	function doSend(send, message, delay) {
+		var timer = setTimeout(function() {
+			send(message, onSendComplete);
+		}, delay);
+		timers.push(timer);
+
+		return delay;
 	}
 
-	function doUpload(burnDelay) {
-		updateProgress(15);
-		var blockSize = 128;
-		var count = Math.ceil(hexData.length / blockSize);
-		burnTotal = 1 + count * 2;
-
-		transmitPacket(d2b(command.ENTER_PROGMODE) + d2b(command.Sync_CRC_EOP), 50);
-		var address = 0;
-		for (var i = 0; i < count; i++) {
-			var block = hexData.substr(blockSize * i, blockSize);
-			transmitPage(address, block, burnDelay);
-			address += 64;
+	function onSendComplete() {
+		packageCount++;
+		if (packageCount == packageTotal) {
+			updateProgress(100);
+			doComplete(true);
+		} else {
+			updateProgress(Math.round(100 * packageCount / packageTotal));
 		}
-		timer = 0;
 	}
 
 	function updateProgress(value) {
@@ -106,54 +84,59 @@ define(function() {
 	}
 
 	function doComplete(value) {
+		timers.forEach(clearTimeout);
+		timers = [];
+
+		packageTotal = 0;
+		packageCount = 0;
+		progressCallback = null;
+
 		completeCallback && completeCallback(value);
+		completeCallback = null;
 	}
 
-	function transmitPage(address, block, delay) {
-		address = hexPad16(address.toString(16));
+	function convertHex(hexRaw) {
+		var result = [];
+
+		var hexData = "";
+		hexRaw.split("\n").forEach(function(line) {
+			var size = parseInt(line.substr(1, 2), 16);
+			if (size == 0) {
+				return true;
+			}
+
+			for (var i = 0; i < 2 * size; i += 2) {
+				hexData += d2b(parseInt(line.substr(i + 9, 2), 16));
+			}
+		});
+
+		var blockSize = 128;
+		var count = Math.ceil(hexData.length / blockSize);
+
+		result.push(d2b(COMMAND.ENTER_PROG_MODE) + d2b(COMMAND.SYNC_CRC_EOP));
+
+		for (var i = 0; i < count; i++) {
+			var block = hexData.substr(blockSize * i, blockSize);
+			var address = i * blockSize >> 1;
+			address = convertAddress(address);
+			result.push(d2b(COMMAND.LOAD_ADDRESS) + address + d2b(COMMAND.SYNC_CRC_EOP));
+			result.push(d2b(COMMAND.PROG_PAGE) + d2b(0x00) + d2b(block.length) + d2b(0x46) + block + d2b(COMMAND.SYNC_CRC_EOP));
+		}
+
+		return result;
+	}
+
+	function convertAddress(address) {
+		address = "0000" + address.toString(16);
+		address = address.substr(address.length - 4);
 		address = address[2] + address[3] + address[0] + address[1];
-		address = String.fromCharCode(parseInt(address[0] + address[1], 16)) + String.fromCharCode(parseInt(address[2] + address[3], 16));
-		transmitPacket(d2b(command.LOAD_ADDRESS) + address + d2b(command.Sync_CRC_EOP), delay);
-		transmitPacket(d2b(command.PROG_PAGE) + d2b(0x00) + d2b(block.length) + d2b(0x46) + block + d2b(command.Sync_CRC_EOP), delay);
-	}
+		address = d2b(parseInt(address[0] + address[1], 16)) + d2b(parseInt(address[2] + address[3], 16));
 
-	function transmitPacket(msg, delay) {
-		setTimeout(function() {
-			chrome.serial.send(connectionId, stringToBuffer(msg), checkComplete);
-		}, delay + timer);
-		timer = timer + delay;
-	}
-
-	function checkComplete() {
-		burnCount++;
-		if(burnCount == burnTotal) {
-			updateProgress(100);
-			doComplete(true);
-		} else {
-			updateProgress(15 + Math.round((burnCount / burnTotal) * 85));
-		}
-	}
-
-	function stringToBuffer(str) {
-		var encodedString = str;
-		var bytes = new Uint8Array(encodedString.length);
-		for (var i = 0; i < encodedString.length; ++i) {
-			bytes[i] = encodedString.charCodeAt(i);
-		}
-		return bytes.buffer;
-	};
-
-	function d2b(number) {
-		return String.fromCharCode(number);
-	}
-
-	function hexPad16(num, size) {
-		var size = 4;
-		var s = "0000" + num;
-		return s.substr(s.length - size);
+		return address;
 	}
 
 	return {
 		exec: exec,
+		convertHex: convertHex,
 	};
 });
