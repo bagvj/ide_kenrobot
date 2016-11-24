5e652a8ae31cf13ca02db9382ff2a571ec5c2b79
kite17 <wangkunpeng@kenrobot.com>
2016-11-25 02:41:32 +0800

    清除无用配置信息

--- config/platform.php
+++ config/platform.php
@@ -2,24 +2,15 @@
 
 return [
     'url' => [
-    	'base' => env("PLATFORM_URL_BASE", "http://www.kenrobot.com/"),
-
     	'register' => "http://www.kenrobot.com/index.php?app=public&mod=Register&act=index",
     	'find_password' => "http://www.kenrobot.com/index.php?app=public&mod=Passport&act=findPassword",
-
-    	'token' => "D4v8dD",
-
-        'saveProject' => "?app=api&mod=Project&act=save",
-        'getProject' => "?app=api&mod=Project&act=item",
-        'getUserProjects' => "?app=api&mod=Project&act=items",
-        'deleteProject' => "?app=api&mod=Project&act=del",
-
-        'setting' => array(
+    ],
+    
+    'setting' => array(
+        'theme' => 'default',
+        'editor' => array(
+            'tabSize' => 4,
             'theme' => 'default',
-            'editor' => array(
-                'tabSize' => 4,
-                'theme' => 'default',
-            ),
         ),
-    ]
+    ),
 ];
