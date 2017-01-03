405842341b1b08f5beb4b0405dbf2ae27f71ef1d
kite17 <wangkunpeng@kenrobot.com>
2017-01-03 11:27:02 +0800

    bug:fix 用户登录

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use App\WebAuth\Broker;
 use App\WebAuth\ApiProxy;
 use Url;
-use Cache;
 use Carbon\Carbon;
+use Session;
 /**
 * 用户登录
 */
@@ -43,7 +43,7 @@ class AuthController extends Controller
 
     public function weixinlogin(Request $request)
     {
-        $ori_login_key = Cache::get('login_key');
+        $ori_login_key = Session::get('login_key');
         $login_key = $request->input('login_key');
         if (empty($ori_login_key) || empty($login_key)) {
             return $this->apiReturn(-3, 'login_key 过期，请刷新重试！');
@@ -68,7 +68,7 @@ class AuthController extends Controller
     public function weixinQrcode(Request $request)
     {
         $refresh = $request->input('refresh');
-        $login_key = Cache::get('login_key');
+        $login_key = Session::get('login_key');
 
         if ($refresh || empty($login_key)) {
 
@@ -84,11 +84,12 @@ class AuthController extends Controller
             }
             $expire_seconds = $result['data']['expire_seconds'];
             $login_key = $result['data']['auth_key'];
-            Cache::put('login_key', $login_key, Carbon::now()->addSeconds($expire_seconds));
-            Cache::put('login_data', json_encode($result['data']), Carbon::now()->addSeconds($expire_seconds));
+
+            Session::put('login_key', $login_key);
+            Session::put('login_data', json_encode($result['data']));
         }
 
-        $login_data = Cache::get('login_data');
+        $login_data = Session::get('login_data');
         $login_data = json_decode($login_data);
 
         return $this->apiReturn(0, '获取成功', $login_data);
@@ -108,6 +109,12 @@ class AuthController extends Controller
     {
         $this->broker->logout();
         return redirect('/');
+    }
+
+    public function jsonLogout()
+    {
+        $this->broker->logout();
+        return $this->apiReturn(0, '退出成功');
     }
 
     public function register(Request $request)
